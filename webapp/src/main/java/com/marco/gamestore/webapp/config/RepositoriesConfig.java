d9e1e0c76bcfd54b8662c7c70c667d2ec07e9f52
Marco San Juan <marco.sanjuan@gmail.com>
2021-02-11 23:06:32 +0100

    Add transactionality configuration

--- webapp/src/main/java/com/marco/gamestore/webapp/config/RepositoriesConfig.java
+++ webapp/src/main/java/com/marco/gamestore/webapp/config/RepositoriesConfig.java
@@ -3,8 +3,10 @@ package com.marco.gamestore.webapp.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"com.marco.gamestore.**.infrastructure.repository"})
 @EntityScan(basePackages = {"com.marco.gamestore.**.infrastructure.repository"})
 public class RepositoriesConfig {
