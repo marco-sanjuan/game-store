61f6660a72558f5a122ccfd6970a2538547c715b
marco <marco.sanjuan@gmail.com>
2025-02-19 22:08:46 +0100

    Fix jakarta and some warnings

--- customer/src/main/java/com/marco/gamestore/customer/domain/CustomerId.java
+++ customer/src/main/java/com/marco/gamestore/customer/domain/CustomerId.java
@@ -1,9 +1,11 @@
 package com.marco.gamestore.customer.domain;
 
 import com.marco.gamestore.shared.domain.model.UuidVO;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value
+@EqualsAndHashCode(callSuper = true)
 public class CustomerId extends UuidVO {
 
     public CustomerId(String uuidString) {
--- customer/src/main/java/com/marco/gamestore/customer/domain/CustomerLoginEmail.java
+++ customer/src/main/java/com/marco/gamestore/customer/domain/CustomerLoginEmail.java
@@ -1,9 +1,11 @@
 package com.marco.gamestore.customer.domain;
 
 import com.marco.gamestore.shared.domain.model.EmailVO;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 @Value
+@EqualsAndHashCode(callSuper = true)
 public class CustomerLoginEmail extends EmailVO {
 
     public CustomerLoginEmail(String value) {
